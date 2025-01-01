337e162854597d9f6e488fb6c4264899851176d4
Mohit Raj <mohitraj12099@gmail.com>
2025-01-01 10:42:15 +0000

    first-commit #1

--- src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div className="flex flex-col justify-center items-center h-screen">
-      <h1 className="text-4xl font-bold">My Name is Mohit Raj</h1>
-      <p className="mt-4 text-lg">I am a software developer specializing in web development and design.</p>
-    </div>
-  )
-}

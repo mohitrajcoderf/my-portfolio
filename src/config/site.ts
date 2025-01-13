f823f64417242294ed86e8d6c97d875941a8ebfd
Mohit Raj <mohitraj12099@gmail.com>
2025-01-13 23:04:33 +0000

    update config file

---
+++ src/config/site.ts
@@ -0,0 +1,13 @@
+export type siteConfig = typeof siteConfig
+
+export const siteConfig = {
+    name: "Mohit",
+    description: "Personal Website of Mohit",
+    url: "https://mohitraj.xyz",
+    ogImage: "https://mohitraj.xyz/og.png",
+    links: {
+        github: "https://github.com/mohitrajcoderf",
+        twitter: "https://twitter.com/mohitraj2546",
+        instagram: "https://instagram.com/mohitraj.tsx",
+    },
+}

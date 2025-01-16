a3d493d39403b81b74c4231a672f351a1a594c0c
Mohit Raj <mohitraj12099@gmail.com>
2025-01-16 20:38:51 +0000

    add: workspace

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,15 +1,15 @@
-import { allPosts } from "content-collections"
-export default function Posts() {
-    return (
-        <ul>
-            {allPosts.map((post) => (
-                <li key={post._meta.path}>
-                    <a href={`/posts/${post._meta.path}`}>
-                        <h3>{post.title}</h3>
-                        <p>{post.summary}</p>
-                    </a>
-                </li>
-            ))}
-        </ul>
-    )
-}
+// import { allPosts } from "content-collections"
+// export default function Posts() {
+//     return (
+//         <ul>
+//             {allPosts.map((post) => (
+//                 <li key={post._meta.path}>
+//                     <a href={`/posts/${post._meta.path}`}>
+//                         <h3>{post.title}</h3>
+//                         <p>{post.summary}</p>
+//                     </a>
+//                 </li>
+//             ))}
+//         </ul>
+//     )
+// }
